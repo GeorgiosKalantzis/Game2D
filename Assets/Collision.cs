6737b3676a5844f0d1555dc7e5a4600cec8bc8e8
GeorgiosKalantzis <georgekalantzis997@gmail.com>
2017-03-25 16:00:07 +0200

    Update Collision.cs

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -5,38 +5,35 @@ using UnityEngine.SceneManagement;
 
 
 public class Collision : MonoBehaviour {
-    public bool flag;
-    public GUISkin message;
+  
+	public bool flag;
+        public GUISkin message;
 
-	
-	void Start () {
+	void Start () 
+	{
+		
         flag = false;
 		
 	}
 	
-
-	void Update () {
+        void OnCollisionEnter2D(Collision2D collision)
+        {
 		
-	} 
-
-    void OnCollisionEnter2D(Collision2D collision)
-    {
         flag = true;
+		
+	}
 
-    }
-
-     void OnGUI()
-    {
-        if (flag == true) {
+	void OnGUI()  
+	{
+        if (flag == true) 
+	{
             GUI.skin = message;
-            
+		
             GUI.Label(new Rect(100, 100, 100, 100), "Collision");
+		
             System.Threading.Thread.Sleep(2000);
+		
             SceneManager.LoadScene(1);
         }
-        
-       
-    }
-
-    
+     }    
 }
