25bb3bed070dea57475824b9e61db10a6569b6ce
GeorgiosKalantzis <georgekalantzis997@gmail.com>
2017-03-25 16:04:30 +0200

    Update InfinitySpike.cs

--- Assets/InfinitySpike.cs
+++ Assets/InfinitySpike.cs
@@ -6,21 +6,21 @@ public class InfinitySpike : MonoBehaviour {
 
     public GameObject respike;
     Vector2 can;
-
 	
-	void Start () {
-        can.y = 2;
-
-		
-	}
+    void Start ()
+    {
+        can.y = 2;	
+    }
 	
-	
-	void Update () {
+    void Update () 
+    {
         can.x = transform.position.x;
+	    
         can.x += 14;
+	    
         transform.position = can;
+	    
         Instantiate(respike, transform.position, transform.rotation);
 
-		
 	}
 }
